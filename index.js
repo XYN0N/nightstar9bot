fcfaa45bf2d42f7204c2b021f4eb8619e90a6940
macs <46033510+XYN0N@users.noreply.github.com>
2025-02-07 15:51:53 +0100

    f

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser');
 const TelegramBot = require('node-telegram-bot-api');
 const { google } = require('googleapis');
 const path = require('path');
+const crypto = require('crypto');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -53,9 +54,59 @@ app.set('views', path.join(__dirname, 'views'));
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 bot.on('polling_error', (error) => console.log(error));
 
+// Gestione del comando /start
+bot.onText(/\/start/, async (msg) => {
+  const chatId = msg.chat.id;
+  const username = msg.from.username;
+  const profilePhoto = await bot.getUserProfilePhotos(chatId).then(p => p.photos[0][0].file_id).catch(() => null);
+
+  // Genera un token univoco per l'utente
+  const token = crypto.randomBytes(16).toString('hex');
+
+  // Salva il token in Redis con una scadenza di 10 minuti
+  redisClient.setex(token, 600, JSON.stringify({ chatId, username, profilePhoto }));
+
+  // Invia il link di login all'utente
+  bot.sendMessage(chatId, `Clicca sul seguente link per accedere alla tua home: https://nightstar9bot.herokuapp.com/login/${token}`);
+});
+
 // Rotte
 app.get('/', (req, res) => {
   res.render('index', { telegramBotToken: process.env.TELEGRAM_BOT_TOKEN });
+});
+
+app.get('/login/:token', (req, res) => {
+  const token = req.params.token;
+
+  // Verifica il token in Redis
+  redisClient.get(token, async (err, data) => {
+    if (err || !data) {
+      return res.status(400).send('Token non valido o scaduto.');
+    }
+
+    const userData = JSON.parse(data);
+
+    // Registra l'utente nel database
+    let user = await User.findOne({ telegramId: userData.chatId });
+    if (!user) {
+      user = new User({
+        telegramId: userData.chatId,
+        username: userData.username,
+        profilePhoto: userData.profilePhoto,
+        stars: 0,
+        walletStars: 0,
+        wins: 0,
+        losses: 0
+      });
+      await user.save();
+    }
+
+    // Salva l'ID utente nella sessione
+    req.session.userId = user._id;
+
+    // Reindirizza alla home
+    res.redirect('/home');
+  });
 });
 
 app.get('/home', (req, res) => {
