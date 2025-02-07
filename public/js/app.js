b99ee4baf4fd4a90a89662cec2f5b4fc92e3679c
macs <46033510+XYN0N@users.noreply.github.com>
2025-02-07 20:32:28 +0100

    f

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = new WebSocket(`wss://${window.location.host}`);
 
 document.getElementById('profile-btn').addEventListener('click', () => {
     displaySection('profile-section');
